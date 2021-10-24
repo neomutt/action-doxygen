ff6d1795b430fc6fe8ca9970ea283d203d14b99c
Richard Russon <rich@flatcap.org>
2021-10-24 15:59:45 +0100

    shellcheck percentages.sh

--- bin/percentages.sh
+++ bin/percentages.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-sort -nr -k2 percentages.txt | while read NAME NUM; do
-	if [ $NUM -gt 49 ]; then
+set -o errexit	# set -e
+set -o nounset	# set -u
+
+BASE_DIR="${0%/*}"
+
+SRC="$BASE_DIR/percentages.txt"
+DST="gfx"
+
+if ! [ -r "$SRC" ]; then
+	echo "Can't read: '$SRC'"
+	exit 1
+fi
+
+if ! [ -r "$DST" ]; then
+	echo "Destination doesn't exist: '$DST'"
+	exit 1
+fi
+
+sort -nr -k2 "$SRC" | while read -r NAME NUM; do
+	if [ "$NUM" -gt 49 ]; then
 		COLOUR=green
-	elif [ $NUM -gt 24 ]; then
+	elif [ "$NUM" -gt 24 ]; then
 		COLOUR=yellow
 	else
 		COLOUR=red
 	fi
-	wget --quiet -O gfx/$NAME.svg https://img.shields.io/badge/${NAME}-${NUM}%-${COLOUR}.svg
-	echo ' * <img style="float: left; padding-right: 0.5em;" src="'$NAME'.svg">'
+	wget --quiet -O "gfx/$NAME.svg" "https://img.shields.io/badge/${NAME}-${NUM}%-${COLOUR}.svg"
+	echo ' * <img style="float: left; padding-right: 0.5em;" src="'"$NAME"'.svg">'
 done
 
